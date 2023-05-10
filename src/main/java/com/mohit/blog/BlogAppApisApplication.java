ed4d8ea53288ac61aafc088fbee4b565f10a87db
mohit <chandmohit163@gmail.com>
2023-05-10 22:58:22 +0530

    basic https authentication

--- src/main/java/com/mohit/blog/BlogAppApisApplication.java
+++ src/main/java/com/mohit/blog/BlogAppApisApplication.java
@@ -1,15 +1,21 @@
 package com.mohit.blog;
 
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 
 @SpringBootApplication
-public class BlogAppApisApplication {
-
+public class BlogAppApisApplication implements CommandLineRunner {
+	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(BlogAppApisApplication.class, args);
 	}
@@ -17,6 +23,11 @@ public class BlogAppApisApplication {
 	@Bean
 	public ModelMapper modelMapper() {
 		return new ModelMapper();
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println(this.passwordEncoder.encode("testing password encoding"));
 	}
 
 }
